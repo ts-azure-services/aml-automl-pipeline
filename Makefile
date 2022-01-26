98f7d48ca7db7125b798a1c5451f437c2526fdb9
Santosh Thomas <thomassantosh@gmail.com>
2022-01-25 21:58:48 -0800

    Changes as of 'Tue Jan 25 21:58:48 PST 2022'

---
+++ Makefile
@@ -0,0 +1,20 @@
+install:
+	pip install --upgrade pip &&\
+		pip install -r requirements.txt
+
+setup_run:
+	./scripts/setup/create-aml-resources.sh
+	python ./scripts/setup/clusters.py
+	python ./scripts/setup/upload_baseline_data.py
+
+lint:
+	pylint --disable=R,C,W1203,W0702,E0110,W0703 ./modeling/retrain/create_retrain_dataset.py
+	pylint --disable=R,C,W1203,W0702,E0110,W0703 ./modeling/conceptdrift/create_concept_dataset.py
+	pylint --disable=R,C,W1203,W0702,E0110,W0703 ./modeling/datadrift/create_datadrift_dataset.py
+
+create_datasets:
+	python ./modeling/retrain/create_retrain_dataset.py
+	python ./modeling/conceptdrift/create_concept_dataset.py
+	python ./modeling/datadrift/create_datadrift_dataset.py
+
+all: install lint
--- scripts/pipeline/register_model.py
+++ scripts/pipeline/register_model.py
@@ -1,6 +1,7 @@
 import argparse
 from authentication import ws
-from azureml.core.model import Model, Dataset
+from azureml.core.model import Model
+from azureml.core import Dataset
 from azureml.core.run import Run, _OfflineRun
 
 def getArgs(argv=None):
@@ -16,10 +17,17 @@ def main():
     print(f'Model path is: {args.model_path}')
 
     run = Run.get_context()
+    prepped_data = Dataset.get_by_name(workspace=ws, name='prepped_data')
 
     # Get best model
-    model = Model.register(workspace=ws, model_path=args.model_path, model_name=args.model_name)
-    print(f"Registered version {0} of model {1}".format(model.version, model.name))
+    model = Model.register(
+            workspace=ws, 
+            model_path=args.model_path, 
+            model_name=args.model_name,
+            sample_input_dataset=prepped_data,
+            description="Housing price AutoML time series forecasting",
+            tags={'type':'time series', 'area':'housing prices'}
+            )
 
 if __name__ == "__main__":
     main()
